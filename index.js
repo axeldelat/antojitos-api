87e0b6e277833345be97acd2fa54d7c6884b5d65
Axel de la Torre <axeldelat@gmail.com>
2020-10-16 20:23:29 -0500

    add db.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// En este archivo se encarga de levantar el todos los servicios
+// el dotenv debe ser la primer linea a ejecutarse
+require('dotenv').config()
+
+console.log('env: ', process.env.DB_USER)
